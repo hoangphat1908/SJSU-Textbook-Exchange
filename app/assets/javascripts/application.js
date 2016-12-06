3c94b3e692a73c9d6b465d2d480c5c60f35903b5
Peter <qnguyen523@gmail.com>
2016-12-06 10:43:20 -0800

    deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui/autocomplete
+//= require twitter/bootstrap
 //= require autocomplete-rails
 //= require turbolinks
 //= require_tree .
