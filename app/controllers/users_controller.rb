6644b8fc8c1e5144ea9c75bcda6843f9c722bc65
Peter <qnguyen523@gmail.com>
2016-12-05 23:08:06 -0800

    add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   # GET /users/1.jsons
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    # @posts = @user.posts
+    @posts = @user.posts.paginate(:page => params[:page], :per_page => 5)
   end
 
   # GET /users/new
